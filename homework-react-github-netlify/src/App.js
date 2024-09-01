a4c2292913009e13ed7096997145bfc54cd794f1
Muri <mcsanchezlaguna@gmail.com>
2024-09-01 20:49:27 +0200

    Create new React app

---
+++ homework-react-github-netlify/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import WeatherApp from "./WeatherApp";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <body>
+        <div className="container">
+          <h3>SheCodes React Week 4 Homework</h3>
+          <h1>React + GitHub + Netlify</h1>
+          <WeatherApp />
+          <WeatherApp />
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ homework-react-github-netlify/src/WeatherApp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WeatherApp() {
+  return (
+    <div>
+      <p>Hola mundo</p>
+    </div>
+  );
+}
