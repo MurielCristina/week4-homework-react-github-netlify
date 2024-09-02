ed5524d4322cd82c95fafaaa880a5737953b7fbf
Muri <mcsanchezlaguna@gmail.com>
2024-09-02 11:56:20 +0200

    added the font and style it

--- homework-react-github-netlify/src/WeatherApp.js
+++ homework-react-github-netlify/src/WeatherApp.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherApp() {
   return (
     <div>
-      <p>Hola mundo</p>
+      <form className="form">
+        <input
+          type="search"
+          className="form-search"
+          placeholder="Enter a city..."
+        />
+        <input type="submit" value="Search" className="form-sumbit" />
+      </form>
     </div>
   );
 }
